ae12a0c4f75fd5c2e995c9ebc092e1d96dee7cde
mechoirunnisa <nisaninda3@gmail.com>
2022-08-01 19:47:28 +0700

    list

--- app/src/main/java/com/choirunnisa/fidac_choirunnisa_test/adapter/ImageAdapter.kt
+++ app/src/main/java/com/choirunnisa/fidac_choirunnisa_test/adapter/ImageAdapter.kt
@@ -5,12 +5,14 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.choirunnisa.fidac_choirunnisa_test.R
 import com.choirunnisa.fidac_choirunnisa_test.databinding.RowDetailPersonBinding
+import com.choirunnisa.fidac_choirunnisa_test.databinding.RowImageBinding
 import com.choirunnisa.fidac_choirunnisa_test.databinding.RowVariantBinding
 import com.choirunnisa.fidac_choirunnisa_test.model.Variants
-import com.choirunnisa.fidac_choirunnisa_test.ui.DetailProductActivity
+import com.squareup.picasso.Picasso
 
-class VariantAdapter(val context: Context) : RecyclerView.Adapter<VariantAdapter.ListViewHolder>(){
+class ImageAdapter(val context: Context) : RecyclerView.Adapter<ImageAdapter.ListViewHolder>(){
 
     var listVariant = mutableListOf<Variants>()
 
@@ -23,24 +25,25 @@ class VariantAdapter(val context: Context) : RecyclerView.Adapter<VariantAdapter
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view = RowVariantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = RowImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
        with(holder){
            with(listVariant[position]){
-               binding.color.setBackgroundColor(Color.parseColor(this.variant_color))
-               binding.color.setOnClickListener {
-                   (context as DetailProductActivity).changeImage(position)
-               }
+               val photoUrl = "https://app.minjem.biz.id/upload/tech_test_image/${this.variant_image}"
+               Picasso.get().load(photoUrl)
+                   .placeholder(R.drawable.avatar)
+                   .resize(120, 200)
+                   .error(R.drawable.avatar).into(binding.img)
            }
        }
     }
 
     override fun getItemCount(): Int = listVariant.size
 
-    inner class ListViewHolder(val binding: RowVariantBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ListViewHolder(val binding: RowImageBinding) : RecyclerView.ViewHolder(binding.root)
 
     }
 
--- app/src/main/java/com/choirunnisa/fidac_choirunnisa_test/ui/DetailProductActivity.kt
+++ app/src/main/java/com/choirunnisa/fidac_choirunnisa_test/ui/DetailProductActivity.kt
@@ -2,12 +2,18 @@ package com.choirunnisa.fidac_choirunnisa_test.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.choirunnisa.fidac_choirunnisa_test.R
+import com.choirunnisa.fidac_choirunnisa_test.adapter.ImageAdapter
 import com.choirunnisa.fidac_choirunnisa_test.adapter.PersonAdapter
 import com.choirunnisa.fidac_choirunnisa_test.adapter.StudiesAdapter
 import com.choirunnisa.fidac_choirunnisa_test.adapter.VariantAdapter
@@ -27,11 +33,12 @@ import java.util.*
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-class DetailProductActivity : AppCompatActivity(), CoroutineScope{
+class DetailProductActivity : AppCompatActivity(), CoroutineScope {
 
-    companion object{
+    companion object {
         const val paramIdProduct = "param_id_product"
     }
+
     private lateinit var job: Job
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
@@ -43,12 +50,13 @@ class DetailProductActivity : AppCompatActivity(), CoroutineScope{
         ViewModelProviders.of(this, viewModelFactory).get(DetailProductViewModel::class.java)
     }
 
-    private var _binding : ActivityDetailProductBinding? = null
-    private val binding get() =  _binding!!
+    private var _binding: ActivityDetailProductBinding? = null
+    private val binding get() = _binding!!
 
     var idProduct = 0
 
     lateinit var variantAdapter: VariantAdapter
+    lateinit var imageAdapter: ImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,8 +69,9 @@ class DetailProductActivity : AppCompatActivity(), CoroutineScope{
         launch {
             try {
                 vm.getTitle(idProduct)
-            }catch (e:Exception){
-                Toast.makeText(this@DetailProductActivity, "gagal load data $e", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(this@DetailProductActivity, "gagal load data $e", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
 
@@ -70,17 +79,24 @@ class DetailProductActivity : AppCompatActivity(), CoroutineScope{
         vm.desc.observe(this, Observer(::onDesc))
         vm.harga.observe(this, Observer(::onPrice))
         vm.image.observe(this, Observer(::onVarian))
+        vm.image.observe(this, Observer(::loadImage))
 
         variantAdapter = VariantAdapter(this)
         binding.varian.apply {
             adapter = variantAdapter
-            layoutManager = LinearLayoutManager(this@DetailProductActivity,  LinearLayoutManager.HORIZONTAL ,false)
+            layoutManager = LinearLayoutManager(
+                this@DetailProductActivity,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
         }
 
         binding.tbNameProduct.setNavigationIcon(R.drawable.ic_arrow_back_black)
         binding.tbNameProduct.setNavigationOnClickListener {
             onBackPressed()
         }
+        imageAdapter = ImageAdapter(this)
+
 
     }
 
@@ -91,24 +107,51 @@ class DetailProductActivity : AppCompatActivity(), CoroutineScope{
         }
     }
 
-    private fun onTitle(result: String?){
+    private fun onTitle(result: String?) {
         result ?: return
         binding.tbNameProduct.title = result
     }
 
-    private fun onDesc(result: String?){
+    private fun onDesc(result: String?) {
         result ?: return
         binding.desc.text = result
     }
 
-    private fun onPrice(result: String?){
+    private fun onPrice(result: String?) {
         result ?: return
-        binding.price.text = NumberFormat.getCurrencyInstance(Locale("id","ID")).format(result.toDouble())
+        binding.price.text =
+            NumberFormat.getCurrencyInstance(Locale("id", "ID")).format(result.toDouble())
     }
 
-    private fun onVarian(result: List<Variants>?){
+    private fun onVarian(result: List<Variants>?) {
         result ?: return
         variantAdapter.addData(result)
         variantAdapter.notifyDataSetChanged()
     }
+
+    private fun loadImage(result: List<Variants>?) {
+        result ?: return
+        imageAdapter.addData(result)
+        imageAdapter.notifyDataSetChanged()
+        binding.slider.adapter = imageAdapter
+        binding.slider.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+            }
+        })
+
+        (binding.slider.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_ALWAYS
+
+
+    }
+
+    fun changeImage(position: Int){
+        (binding.slider.getChildAt(1) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_ALWAYS
+    }
+
+
 }
